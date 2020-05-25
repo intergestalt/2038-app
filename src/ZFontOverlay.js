3d0bc9574e1234a9a1e1f0e4feeb134899385fa8
Holger H <holgheiss@gmail.com>
2020-05-25 22:49:27 +0200

    clean up

--- src/ZFontOverlay.js
+++ src/ZFontOverlay.js
@@ -251,28 +251,6 @@ export default class ZFontOverlay extends React.Component {
     return resultCanvas
   }
 
-  /*
-  onDeviceOrientationChangeEvent(event) {
-
-    this.setState({orientation: event});
-    if(this.illo) this.setState({rotate: this.illo.rotate});
-    if(this.dragging) return;
-
-    if(!this.alphaOffset) this.alphaOffset = event.alpha;
-
-    if(this.illo) {
-
-      const {alpha, beta, gamma} = event
-
-      this.illo.rotate.y = compassHeading( alpha, beta, gamma ) //( -event.gamma ) * Math.PI/180
-      this.illo.rotate.x = 0//getQuaternion( alpha, beta, gamma )[1] *Math.PI //(  event.beta ) * Math.PI/180
-      this.illo.rotate.z = 0//(  event.alpha - 180 ) * Math.PI/180 
-      
-    }
-  }
-
-  */
-
   render() {
     return( 
       <VideoContainer initialized={this.state.initialized}>
@@ -354,67 +332,4 @@ const SensorInfo = styled.div`
     
   
 
-
-
-
-
-
 var degtorad = Math.PI / 180; // Degree-to-Radian conversion
-
-function compassHeading( alpha, beta, gamma ) {
-
-  var _x = beta  ? beta  * degtorad : 0; // beta value
-  var _y = gamma ? gamma * degtorad : 0; // gamma value
-  var _z = alpha ? alpha * degtorad : 0; // alpha value
-
-  var cX = Math.cos( _x );
-  var cY = Math.cos( _y );
-  var cZ = Math.cos( _z );
-  var sX = Math.sin( _x );
-  var sY = Math.sin( _y );
-  var sZ = Math.sin( _z );
-
-  // Calculate Vx and Vy components
-  var Vx = - cZ * sY - sZ * sX * cY;
-  var Vy = - sZ * sY + cZ * sX * cY;
-
-  // Calculate compass heading
-  var compassHeading = Math.atan( Vx / Vy );
-
-  // Convert compass heading to use whole unit circle
-  if( Vy < 0 ) {
-    compassHeading += Math.PI;
-  } else if( Vx < 0 ) {
-    compassHeading += 2 * Math.PI;
-  }
-
-  //return compassHeading * ( 180 / Math.PI ); // Compass Heading (in degrees)
-  return compassHeading; // Compass Heading (in rad)
-
-}
-
-function getQuaternion( alpha, beta, gamma ) {
-
-  var _x = beta  ? beta  * degtorad : 0; // beta value
-  var _y = gamma ? gamma * degtorad : 0; // gamma value
-  var _z = alpha ? alpha * degtorad : 0; // alpha value
-
-  var cX = Math.cos( _x/2 );
-  var cY = Math.cos( _y/2 );
-  var cZ = Math.cos( _z/2 );
-  var sX = Math.sin( _x/2 );
-  var sY = Math.sin( _y/2 );
-  var sZ = Math.sin( _z/2 );
-
-  //
-  // ZXY quaternion construction.
-  //
-
-  var w = cX * cY * cZ - sX * sY * sZ;
-  var x = sX * cY * cZ - cX * sY * sZ;
-  var y = cX * sY * cZ + sX * cY * sZ;
-  var z = cX * cY * sZ + sX * sY * cZ;
-
-  return [ w, x, y, z ];
-
-}
